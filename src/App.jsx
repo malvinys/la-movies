cfeb75b3cafb188068e18f9c30864e43e61101f3
Malvin YS <malvinyosef@gmail.com>
2021-05-29 14:44:50 +0700

    add package react-helmet and set document title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import './assets/saas/App.scss';
 import Routes from './routes/Routes';
@@ -7,6 +8,9 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="base-container">
+      <Helmet>
+        <title>LA-Movies</title>
+      </Helmet>
       <Header />
       <Routes />
     </div>
